255e5e3698266490dafb740717c1d59218894259
MrDalo <dadokralik@gmail.com>
2023-11-15 23:27:26 +0100

    feat: Created ASC/DESC sorting for each column | Created fixed table header

--- nextjs-app/src/app/table/data-table.tsx
+++ nextjs-app/src/app/table/data-table.tsx
@@ -4,7 +4,9 @@ import {
 	ColumnDef,
 	flexRender,
 	getCoreRowModel,
-	useReactTable
+	useReactTable,
+	SortingState,
+	getSortedRowModel
 } from '@tanstack/react-table';
 
 import {
@@ -15,6 +17,7 @@ import {
 	TableHeader,
 	TableRow
 } from '@/components/ui/table';
+import { useState } from 'react';
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
@@ -25,24 +28,35 @@ export function DataTable<TData, TValue>({
 	columns,
 	data
 }: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([]);
+
 	const table = useReactTable({
 		data,
 		columns,
-		getCoreRowModel: getCoreRowModel()
+		getCoreRowModel: getCoreRowModel(),
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		state: {
+			sorting
+		}
 	});
 
 	return (
-		<div className="h-[80vh] overflow-auto rounded-md">
+		<div className="relative h-[80vh] overflow-auto">
 			<Table>
-				<TableHeader>
+				<TableHeader className="sticky top-0">
 					{table.getHeaderGroups().map(headerGroup => (
 						<TableRow
 							key={headerGroup.id}
-							className={` border-[#4d4d52] hover:bg-[#292929]`}
+							className={` border-[#4d4d52] bg-[#09090b] hover:bg-[#09090b]`}
+							// className={` border-[#4d4d52] hover:bg-[#292929]`}
 						>
 							{headerGroup.headers.map(header => {
 								return (
-									<TableHead key={header.id} className={` text-[#96969E]`}>
+									<TableHead
+										key={header.id}
+										className={` sticky top-0 text-[#96969E]`}
+									>
 										{header.isPlaceholder
 											? null
 											: flexRender(
@@ -64,7 +78,10 @@ export function DataTable<TData, TValue>({
 								className={` border-[#4d4d52] hover:bg-[#292929]`}
 							>
 								{row.getVisibleCells().map(cell => (
-									<TableCell key={cell.id} className={` text-[#EEEEEE]`}>
+									<TableCell
+										key={cell.id}
+										className={` text-center text-[#EEEEEE]`}
+									>
 										{flexRender(cell.column.columnDef.cell, cell.getContext())}
 									</TableCell>
 								))}
